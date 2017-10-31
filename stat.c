96a846b36cec40917a76686017f567e5c83a54ee
Jen Yu <jyu7@stuy.edu>
2017-10-31 17:12:32 -0400

    conveters mainly work?:

--- stat.c
+++ stat.c
@@ -3,9 +3,28 @@
 #include <time.h>
 #include <sys/stat.h>
 
-char* det_size(long size)
+int dec_to_oct(int num)
 {
-  
+  if (num < 8)
+    {
+      return num;
+    }
+  else
+    {
+      return num%8+ 10*dec_to_oct(num/8);
+    }
+}
+
+long oct_to_bin(int num)
+{
+  if (num < 2)
+    {
+      return num;
+    }
+  else
+    {
+      return num%2+10*oct_to_bin(num/2);
+    }
 }
 
 int main () {
@@ -18,8 +37,11 @@ int main () {
     size /= 1024;
     ctr++;
   }
+
   printf("size of stat.c: %f %s\n", size, units[ctr]);
-  printf("mode of stat.c: %o\n", sb.st_mode);
+  printf("mode of stat.c: %d\n",sb.st_mode);
+  printf("mode of stat.c: %d\n",dec_to_oct(sb.st_mode));
+  printf("mode of stat.c: %ld\n",oct_to_bin(dec_to_oct(sb.st_mode)));
   printf("last accessed time of stat.c: %s\n", ctime( &sb.st_atime ) );
   return 0;
 }
