00437150ec447c308affae0f08010de2478c51dc
adeeburrahman <23088430+adeeburrahman@users.noreply.github.com>
2017-10-31 15:52:09 -0400

    .

--- stat.c
+++ stat.c
@@ -6,8 +6,10 @@
 int main () {
   struct stat sb;
   stat("stat.c", &sb);
-  printf("size of stat.c in bytes: %lu\n", sb.st_size);
-  printf("mode of stat.c: %d\n", sb.st_mode);
+  char str[80];
+  sprintf(str, "size of stat.c in bytes: %lu\n", sb.st_size);
+  printf("%s", str);
+  printf("mode of stat.c: %o\n", sb.st_mode);
   printf("last accessed time of stat.c: %s\n", ctime( &sb.st_atime ) );
   return 0;
 }
