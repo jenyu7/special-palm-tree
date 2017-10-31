bcf5db2ef7900e1b1d25cda99bc6f5aaa9a49c8d
Jen Yu <jyu7@stuy.edu>
2017-10-31 16:16:25 -0400

    human readable file size

--- stat.c
+++ stat.c
@@ -3,12 +3,22 @@
 #include <time.h>
 #include <sys/stat.h>
 
+char* det_size(long size)
+{
+  
+}
+
 int main () {
   struct stat sb;
   stat("stat.c", &sb);
-  char str[80];
-  sprintf(str, "size of stat.c in bytes: %lu\n", sb.st_size);
-  printf("%s", str);
+  char * units[] = {"B", "KB", "MB", "GB"};
+  int ctr = 0;
+  float size = (float)sb.st_size;
+  while (size > 1024) {
+    size /= 1024;
+    ctr++;
+  }
+  printf("size of stat.c: %f %s\n", size, units[ctr]);
   printf("mode of stat.c: %o\n", sb.st_mode);
   printf("last accessed time of stat.c: %s\n", ctime( &sb.st_atime ) );
   return 0;
